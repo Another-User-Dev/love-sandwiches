9075a3457a8c5ba425e8b94aa3d691cd6f414dad
Michael Mohamed <michael.jobsearch.emails@gmail.com>
2024-08-03 21:46:26 +0000

    Add calculated surplus data to surplus worksheet

--- run.py
+++ run.py
@@ -16,14 +16,14 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
     Run a while loop to collect a valid string of data from the user
     via the terminal, which must be a string of 6 numbers separated
     by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
-        print("Data should be six numbers, separated by commas")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10, 20, 30, 40, 50, 60\n")
 
         data_str = input("Enter your data here: ")
@@ -77,6 +77,7 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
+    print(stock_row)
     
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
@@ -88,7 +89,7 @@ def calculate_surplus_data(sales_row):
 
 def main():
     """
-    Run all program function
+    Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
